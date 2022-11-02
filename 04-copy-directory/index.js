3bf035eb39e33e4f20dc5f9c2d90c9423814072b
brbrov <epoc@list.ru>
2022-11-02 20:18:21 +0300

    feat: create 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+let way = path.resolve(__dirname, 'files-copy');
+let copyPath = path.resolve(__dirname, 'files');
+
+let toDo = async function(way, copyPath){
+    let res = await fs.mkdir(way, {recursive: true});
+    if(!res){
+        await fs.rm(way, {recursive: true});
+        await fs.mkdir(way);
+    }
+
+    let arr = await fs.readdir(copyPath, {withFileTypes: true});    
+
+    for (const dirent of arr){
+        let pathScr = path.join(copyPath, dirent.name);  
+        let pathFile = path.join(way, dirent.name);      
+        fs.copyFile(pathScr, pathFile);
+    }
+}
+
+toDo(way, copyPath);
+
 
